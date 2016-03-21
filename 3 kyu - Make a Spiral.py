0d32e11f23507abaff41de36c6b55a3399be7897
Vincent QB <vincentqb@users.noreply.github.com>
2016-03-21 14:19:14 -0400

    new more compact solution

--- 3 kyu - Make a Spiral.py
+++ 3 kyu - Make a Spiral.py
@@ -1,3 +1,30 @@
+def can_move(map, i, j, di, dj):
+  n = len(map)
+  i += di
+  j += dj
+  
+  # Cannot move outside of map
+  if i < 0 or i >= n or j < 0 or j >= n:
+    return False
+
+  # Cannot move if occupied
+  if map[i][j] == 1:
+    return False
+    
+  i += di
+  j += dj
+  
+  # Can move if second move falls outside
+  if i < 0 or i >= n or j < 0 or j >= n:
+    return True
+  
+  # Cannot move if second move is occupied
+  if map[i][j] == 1:
+    return False
+    
+  # Otherwise we can move
+  return True
+
 def spiralize(size):
 
   if size == 0:
@@ -7,46 +34,38 @@ def spiralize(size):
   elif size == 2:
     return [[1,1],[0,1]]
     
+  # Initial map
   spiral = [ [0 for i in range(size)] for j in range(size) ]
   
-  # Make a snake
-  iter = 0
+  # Start position
   i = j = 0
-  changed = True
-  while changed:
-    changed = False
-    spiral[i][j] = 1
-    if iter % 4 == 0:
-      jold = j
-      while j == size-2 or (j < size-2 and spiral[i][j+2] == 0): 
-        j += 1
-        spiral[i][j] = 1
-        changed = True
-      if abs(j - jold) == 1: break
-    elif iter % 4 == 1:
-      iold = i
-      while i == size-2 or (i < size-2 and spiral[i+2][j] == 0):
-        i += 1
-        spiral[i][j] = 1
-        changed = True
-      if abs(i - iold) == 1: break
-    elif iter % 4 == 2:
-      jold = j
-      while j == 1 or (j >= 2 and spiral[i][j-2] == 0):
-        j -= 1
-        spiral[i][j] = 1
-        changed = True
-      if abs(j - jold) == 1: break
-    elif iter % 4 == 3:
-      iold = i
-      while i == 1 or (i >= 2 and spiral[i-2][j] == 0):
-        i -= 1
-        spiral[i][j] = 1
-        changed = True
-      if abs(i - iold) == 1: break
-    iter += 1
-  return spiral
+  
+  # Direction vector
+  di, dj = 0, 1
+  
+  # Make a snake
+  rotated = 0
+  while rotated < 2:
+    # print(i,j)
+    # print(spiral)
 
+    spiral[i][j] = 1
+    if can_move(spiral, i, j, di, dj):
+      # Move
+      i += di
+      j += dj
+      rotated = 0
+    else:
+      # Rotate direction clockwise
+      di, dj = dj, -di
+      rotated += 1
+
+  # Last step might have made spiral curl on itself
+  di, dj = -dj, di
+  if spiral[i + di][j + dj] == 1:
+      spiral[i][j] = 0
+      
+  return spiral
 
 print(spiralize(1) == [[1]])
 print(spiralize(2) == [[1,1], [0,1]])
